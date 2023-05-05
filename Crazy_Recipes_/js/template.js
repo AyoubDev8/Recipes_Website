d207851f9351db2df6ecc2fcb9f63b4ae72ad186
AyoubDev8 <126176740+AyoubDev8@users.noreply.github.com>
2023-05-05 18:11:24 +0100

    Update template.js

--- Crazy_Recipes_/js/template.js
+++ Crazy_Recipes_/js/template.js
@@ -1,23 +1,3 @@
-const getElement = (selector) => {
-  const element = document.querySelector(selector)
-
-  if (element) return element
-  throw Error(
-    `Please double check your class names, there is no ${selector} class`
-  )
-}
-
-const links = getElement('.nav-links')
-const navBtnDOM = getElement('.nav-btn')
-
-navBtnDOM.addEventListener('click', () => {
-  links.classList.toggle('show-links')
-})
-
-const date = getElement('#date')
-const currentYear = new Date().getFullYear()
-date.textContent = currentYear
-
 // Get the query parameters from the URL
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get('category');
